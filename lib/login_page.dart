069be96cd5cb7c3525ce2c845c9790487c2c59f5
Juwel Sheikh <jewelshekh1996@gmail.com>
2022-05-16 00:53:32 +0600

    switch work

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_switch/flutter_switch.dart';
@@ -6,6 +5,7 @@ import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:kadad_customer_design/custom_switch.dart';
 import 'package:kadad_customer_design/page2.dart';
 import 'package:rolling_switch/rolling_switch.dart';
+
 class ExerciseOne extends StatefulWidget {
   const ExerciseOne({Key? key}) : super(key: key);
 
@@ -14,15 +14,20 @@ class ExerciseOne extends StatefulWidget {
 }
 
 class _ExerciseOneState extends State<ExerciseOne> {
-  TextEditingController _phoneController=TextEditingController();
-  TextEditingController _passwordController=TextEditingController();
-  TextEditingController _emailController=TextEditingController();
-  bool switch_bool=false;
-  bool abc=false;
+  TextEditingController _phoneController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  bool switch_bool = false;
+  bool abc = false;
+  bool _switchvalue = false;
   @override
+  void initState() {
+    super.initState();
+    _switchvalue = false;
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -30,139 +35,145 @@ class _ExerciseOneState extends State<ExerciseOne> {
               height: 191,
               width: double.infinity,
               color: Colors.grey.shade100,
-              child:Center(child: SvgPicture.asset("assets/Artboard.svg")) ,
+              child: Center(child: SvgPicture.asset("assets/Artboard.svg")),
             ),
-            SizedBox(height: 8,),
+            SizedBox(
+              height: 8,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Padding(
                   padding: const EdgeInsets.all(20.0),
-                  child: Text("Email or Mobile",style: TextStyle(color: Colors.grey,fontSize:24 ),),
+                  child: Text(
+                    "Email or Mobile",
+                    style: TextStyle(color: Colors.grey, fontSize: 24),
+                  ),
                 ),
                 Padding(
                   padding: const EdgeInsets.all(20.0),
                   child: SizedBox(
                     width: 50,
                     height: 23,
-                    child:
-                    FlutterSwitch(
-                      activeColor: Colors.orange,
+                    child: FlutterSwitch(
+                        activeColor: Colors.orange,
                         inactiveToggleColor: Colors.orange,
                         inactiveColor: Colors.black,
                         activeToggleColor: Colors.black,
                         toggleSize: 16,
-
-                        value: abc,
-                         onToggle: (bool value) {
-                           setState(() {
-                             abc = value;
-                             print("VALUE:$value");
-                           },
-
-
-                           );
-                         }
-
-
+                        value: _switchvalue,
+                        onToggle: (value) {
+                          setState(
+                            () {
+                              _switchvalue = value;
+                            },
+                          );
+                          print(value);
+                        }),
                   ),
-
-
                 ),
 
-                ),
-                Text(abc==false? "First textfield":"Second textfield"),
-
+                // Text(abc==false? "First textfield":"Second textfield"),
               ],
             ),
 
+            _switchvalue == false
+                ? Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: Form(
+                      autovalidateMode: AutovalidateMode.always,
+                      child: TextFormField(
+                        controller: _emailController,
+                        validator: (value) => validateEmail(value),
+                        decoration: InputDecoration(
+                          border: UnderlineInputBorder(),
+                          hintText: 'Customer@login.com',
+                          hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                          icon: Icon(Icons.alternate_email_rounded),
+                          // prefix: SvgPicture.asset("assets/atthe.svg"),
+                          // suffix: SvgPicture.asset("assets/ok.svg")
+                        ),
+                      ),
+                    ),
+                  )
+                :
+                // Container(
+                //   width: 70,
+                //   child: const TextField (
+                //     decoration: InputDecoration(
+                //         border: OutlineInputBorder(),
+                //         labelText: 'Email',
+                //         hintText: 'Enter Your Email'
+                //     ),
+                //   ),
+                // )
 
-
-
-
-            Padding(
-              padding: const EdgeInsets.only(left: 20,right: 20),
-
-              child: TextFormField(
-
-                controller: _emailController,
-                decoration:  InputDecoration(
-                    border: UnderlineInputBorder(),
-                    hintText: 'Customer@login.com',
-                    hintStyle: TextStyle(fontWeight: FontWeight.bold),
-                   icon: Icon(Icons.alternate_email_rounded),
-                  // prefix: SvgPicture.asset("assets/atthe.svg"),
-                  // suffix: SvgPicture.asset("assets/ok.svg")
-
-                ),
-              ),
-            ),
-            SizedBox(height: 2,),
-
-            Container(
-              height: 56,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(5)),
-              ),
-              child: Padding(
-                padding:  EdgeInsets.all(10.0),
-                child: IntlPhoneField(
-                  controller: _phoneController,
-                  decoration: InputDecoration(
-                    hintText: 'XXXXXXXXXXX',
-                    icon: Icon(Icons.local_phone_rounded),
+                Padding(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: IntlPhoneField(
+                      controller: _phoneController,
+                      decoration: InputDecoration(
+                        hintText: 'XXXXXXXXXXX',
+                        icon: Icon(Icons.local_phone_rounded),
+                      ),
+                      onChanged: (phone) {
+                        print(phone.completeNumber);
+                      },
+                    ),
                   ),
-                  onChanged: (phone) {
-                    print(phone.completeNumber);
-                  },
 
-                ),
-              ),
+            // Container(
+            //   width: 70,
+            //   child: const TextField (
+            //     decoration: InputDecoration(
+            //         border: OutlineInputBorder(),
+            //         labelText: 'Password',
+            //         hintText: 'Enter Your Password'
+            //     ),
+            //   ),
+            // ),
+
+            SizedBox(
+              height: 8,
             ),
-
-            SizedBox(height: 8,),
             Padding(
-              padding: const EdgeInsets.only(left: 20,right: 20),
-
+              padding: const EdgeInsets.only(left: 20, right: 20),
               child: TextFormField(
                 controller: _passwordController,
                 obscureText: true,
-
-
                 decoration: InputDecoration(
-                    border: UnderlineInputBorder(),
-                    hintText: '***************************',
-                    hintStyle: TextStyle(),
+                  border: UnderlineInputBorder(),
+                  hintText: '***************************',
+                  hintStyle: TextStyle(),
                   icon: Icon(Icons.lock_outline),
                   // prefix: SvgPicture.asset("assets/lock.svg")
-
                 ),
               ),
             ),
-            SizedBox(height: 8,),
-
-
-
-
+            SizedBox(
+              height: 8,
+            ),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 Container(
-                    margin: EdgeInsets.only(left: 20,right: 20),
-                    child: Text("Forgot password?",style: TextStyle(color: Colors.orangeAccent)))
+                    margin: EdgeInsets.only(left: 20, right: 20),
+                    child: Text("Forgot password?",
+                        style: TextStyle(color: Colors.orangeAccent)))
               ],
             ),
-            SizedBox(height: 12,),
-
+            SizedBox(
+              height: 12,
+            ),
 
             InkWell(
               onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => Page2()));
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => Page2()));
               },
               child: Container(
-                margin: EdgeInsets.only(left: 20,right: 20),
+                margin: EdgeInsets.only(left: 20, right: 20),
                 height: 70.0,
                 decoration: new BoxDecoration(
                   color: Colors.orangeAccent,
@@ -172,47 +183,48 @@ class _ExerciseOneState extends State<ExerciseOne> {
                 child: Center(
                   child: Text(
                     'login',
-                    style:
-                    new TextStyle(fontSize: 18.0,),
+                    style: new TextStyle(
+                      fontSize: 18.0,
+                    ),
                   ),
                 ),
               ),
-
             ),
-            SizedBox(height: 8,),
-
-            Row(
-                children: <Widget>[
-                  Expanded(
-                      child: Divider(
-                        indent: 10,
-                        endIndent: 8,
-                      )
-                  ),
-
-                  Text("OR",style: TextStyle(fontWeight:FontWeight.bold,fontSize: 16),),
-
-                  Expanded(
-                      child: Divider(
-                        indent: 10,
-                        endIndent: 8,
-                      )
-                  ),
-                ]
+            SizedBox(
+              height: 8,
             ),
 
-            SizedBox(height: 8,),
+            Row(children: <Widget>[
+              Expanded(
+                  child: Divider(
+                indent: 10,
+                endIndent: 8,
+              )),
+              Text(
+                "OR",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              Expanded(
+                  child: Divider(
+                indent: 10,
+                endIndent: 8,
+              )),
+            ]),
+
+            SizedBox(
+              height: 8,
+            ),
 
             InkWell(
               onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => Page2()));
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => Page2()));
               },
               child: Container(
-                margin: EdgeInsets.only(left: 20,right: 20),
+                margin: EdgeInsets.only(left: 20, right: 20),
                 height: 70.0,
-                decoration:  BoxDecoration(
-                  border:  Border.all(color: Colors.grey, width: 2.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 2.0),
                   borderRadius: BorderRadius.circular(10.0),
                 ),
                 child: Center(
@@ -220,42 +232,43 @@ class _ExerciseOneState extends State<ExerciseOne> {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Row(
-
                         children: [
                           Padding(
                             padding: const EdgeInsets.all(12.0),
                             child: SvgPicture.asset("assets/google.svg"),
                           ),
-                          SizedBox(width: 17,),
+                          SizedBox(
+                            width: 17,
+                          ),
                           Text(
                             'Continue with google',
-                            style:
-                            new TextStyle(fontSize: 18.0,fontWeight: FontWeight.bold),
+                            style: new TextStyle(
+                                fontSize: 18.0, fontWeight: FontWeight.bold),
                           ),
                         ],
                       ),
-
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Icon(Icons.arrow_forward),
                       )
-
                     ],
                   ),
                 ),
               ),
             ),
-            SizedBox(height: 12,),
+            SizedBox(
+              height: 12,
+            ),
             InkWell(
               onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => Page2()));
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (context) => Page2()));
               },
               child: Container(
-                margin: EdgeInsets.only(left: 20,right: 20),
+                margin: EdgeInsets.only(left: 20, right: 20),
                 height: 70.0,
-                decoration:  BoxDecoration(
-                  border:  Border.all(color: Colors.grey, width: 2.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 2.0),
                   borderRadius: BorderRadius.circular(10.0),
                 ),
                 child: Center(
@@ -268,39 +281,53 @@ class _ExerciseOneState extends State<ExerciseOne> {
                             padding: const EdgeInsets.all(10.0),
                             child: SvgPicture.asset("assets/Apple.svg"),
                           ),
-                          SizedBox(width: 17,),
+                          SizedBox(
+                            width: 17,
+                          ),
                           Text(
                             'Continue with Apple',
-                            style:
-                            new TextStyle(fontSize: 18.0,fontWeight: FontWeight.bold),
+                            style: new TextStyle(
+                                fontSize: 18.0, fontWeight: FontWeight.bold),
                           ),
                         ],
                       ),
-
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Icon(Icons.arrow_forward),
                       )
-
                     ],
                   ),
                 ),
               ),
-
             ),
-            SizedBox(height: 22,),
+            SizedBox(
+              height: 22,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text("Don't have an account?"),
-                Text("Register",style: TextStyle(color: Colors.orangeAccent),)
+                Text(
+                  "Register",
+                  style: TextStyle(color: Colors.orangeAccent),
+                )
               ],
             )
-
-
           ],
         ),
       ),
     );
   }
+}
+
+String validateEmail(String? value) {
+  String pattern =
+      r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+      r"{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]"
+      r"{0,253}[a-zA-Z0-9])?)*$";
+  RegExp regex = RegExp(pattern);
+  if (value == null || value.isEmpty || !regex.hasMatch(value))
+    return 'Enter a valid email address';
+  else
+    return "match";
 }
